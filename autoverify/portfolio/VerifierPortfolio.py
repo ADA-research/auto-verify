f99fdb0ef8f2d8d19cc87e047e4df44cce7af664
c <corne.spek15@gmail.com>
2023-01-09 13:31:21 +0100

    add VerifierPortfolio class

---
+++ autoverify/portfolio/VerifierPortfolio.py
@@ -0,0 +1,17 @@
+from ConfigSpace import ConfigurationSpace
+
+# from ..verifiers import VerifierConfiguration
+
+
+class VerifierPortfolio:
+    def __init__(self):
+        self.portfolio = []
+
+    def append_configuration(configuration: ConfigurationSpace):
+        pass
+
+    def write_to_json(self):
+        pass
+
+    def load_from_json(self):
+        pass
