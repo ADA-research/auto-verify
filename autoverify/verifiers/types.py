11e8740f770b9429e8c34e3d4611b9037f3e52f9
c <corne.spek15@gmail.com>
2023-03-31 14:39:59 +0200

    remove capital letters

--- autoverify/verifiers/types.py
+++ autoverify/verifiers/types.py
@@ -1,13 +1,14 @@
+"""Types to be used with verifiers."""
+
 from typing import Literal
 
 from result import Result
 
-VerifierKind = Literal["Complete", "Incomplete"]
+VerifierKind = Literal["complete", "incomplete"]
 
 Satisfiable = Literal["SAT", "UNSAT"]
 CounterExample = str | None  # TODO: counterexample type (not just `str`)
 
 CompleteVerificationOutcome = tuple[Satisfiable, CounterExample]
-
 # TODO: error type (not just `str`)
 CompleteVerificationResult = Result[CompleteVerificationOutcome, str]
