3b8205368616bd85022041286b924649197e97f6
c <corne.spek15@gmail.com>
2023-03-31 15:26:52 +0200

    move cv-outcome to dataclass

--- autoverify/verifiers/verifier.py
+++ autoverify/verifiers/verifier.py
@@ -3,7 +3,7 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
-from autoverify.verifiers.types import CompleteVerificationResult
+from autoverify.verifiers.verification_result import CompleteVerificationResult
 
 
 @dataclass
