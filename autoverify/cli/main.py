e9befae9a500213cc9a832291fef27d09abda3b9
c <corne.spek15@gmail.com>
2023-04-03 18:01:38 +0200

    add placeholder cli args

--- autoverify/cli/main.py
+++ autoverify/cli/main.py
@@ -1,9 +1,35 @@
+"""CLI entry point module."""
+import argparse
+import sys
+
 import autoverify
 
 
+def build_arg_parser() -> argparse.ArgumentParser:
+    """Setup the cli arg options."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-V", "--version", action="version", version=autoverify.__version__
+    )
+    # TODO: set choices to real verifiers
+    parser.add_argument(
+        "--install",
+        choices=["a", "b", "c"],
+        help="install specified verifiers and exit",
+    )
+
+    return parser
+
+
 def main():
-    print("Hello")
-    print(f"Version {autoverify.__version__}")
+    """Parse and process cli args."""
+    parser = build_arg_parser()
+
+    # print the help message if no args were specified
+    args = parser.parse_args(args=None if sys.argv[1:] else ["--help"])
+    print(args)
+
     return 1
 
 
