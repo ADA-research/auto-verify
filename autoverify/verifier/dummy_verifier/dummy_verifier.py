d87a6de186ef8fedd94b937e631485c1594e4b2f
c <corne.spek15@gmail.com>
2023-04-03 11:52:10 +0200

    silence warnings

--- autoverify/verifier/dummy_verifier/dummy_verifier.py
+++ autoverify/verifier/dummy_verifier/dummy_verifier.py
@@ -1,4 +1,6 @@
-from result import Err, Ok
+"""Temporary dummy verifier."""
+
+from result import Ok
 
 from autoverify.verifier import CompleteVerifier
 from autoverify.verifier.verification_result import (
@@ -8,9 +10,17 @@ from autoverify.verifier.verification_result import (
 
 
 class DummyVerifier(CompleteVerifier):
+    """_summary_."""
+
     def verify_property(self, property, network) -> CompleteVerificationResult:
+        """_summary_."""
+        # temp silence warnings
+        property, network = 0, 0
+        property, network = network, property
+
         outcome = CompleteVerificationOutcome("SAT", None)
         return Ok(outcome)
 
     def sample_configuration(self, config_levels: set[int], size: int):
+        """_summary_."""
         return super().sample_configuration(config_levels, size)
