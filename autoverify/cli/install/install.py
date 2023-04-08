93051c4d3209f0cd6495812f500388c5fcef9eae
c <corne.spek15@gmail.com>
2023-04-08 18:39:08 +0200

    WIP setup error and success messages, make some functions private

--- autoverify/cli/install/install.py
+++ autoverify/cli/install/install.py
@@ -6,15 +6,24 @@ XDG_DATA_HOME = xdg_data_home()
 AV_HOME = XDG_DATA_HOME / "autoverify"
 
 
-def install_verifier(verifier: str) -> Result[None, str]:
+def _install_verifier(verifier: str) -> Result[None, str]:
     """_summary_."""
     if verifier == "DummyVerifier":
+        # Do some installing, and if it all goes well:
         return Ok()
 
-    return Err("Unknown verifier")
+    return Err("Oops")
 
 
+# TODO: Real logging, not prints
 def try_install_verifiers(verifiers: list[str]):
     """_summary_."""
     for verifier in verifiers:
-        install_result = install_verifier(verifier)
+        print(f"Installing {verifier}...")
+
+        install_result = _install_verifier(verifier)
+
+        if isinstance(install_result, Ok):
+            print(f"Succesfully installed {verifier}")
+        elif isinstance(install_result, Err):
+            print(f"Error installing {verifier}: \n{install_result.err()}")
--- autoverify/cli/main.py
+++ autoverify/cli/main.py
@@ -2,16 +2,13 @@
 import argparse
 import sys
 
-from result import Err, Ok
-
 from autoverify import __version__ as AV_VERSION
-from autoverify.cli.install.install import try_install_verifiers
 from autoverify.util.verifiers import get_all_complete_verifier_names
 
 from .install import try_install_verifiers
 
 
-def build_arg_parser() -> argparse.ArgumentParser:
+def _build_arg_parser() -> argparse.ArgumentParser:
     """Setup the cli arg options."""
     parser = argparse.ArgumentParser()
 
@@ -29,21 +26,11 @@ def build_arg_parser() -> argparse.ArgumentParser:
 
 def main():
     """Parse and process cli args."""
-    parser = build_arg_parser()
-
-    # print the help message if no args were specified
+    parser = _build_arg_parser()
     args = parser.parse_args(args=None if sys.argv[1:] else ["--help"])
 
     if args.install:
         try_install_verifiers(args.install)
-        # install_result = install_verifiers(args.install)
-        #
-        # # TODO: real logging
-        # if isinstance(install_result, Ok):
-        #     print(f"Success, {install_result.value}")
-        # elif isinstance(install_result, Err):
-        #     print(f"Failure, {install_result.value}")
-        #
 
 
 if __name__ == "__main__":
