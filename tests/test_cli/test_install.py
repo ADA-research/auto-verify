884f097a2963d820180d880148c965ae7312de48
c <corne.spek15@gmail.com>
2023-04-11 12:20:45 +0200

    version test

--- tests/test_cli/test_install.py
+++ tests/test_cli/test_install.py
@@ -1,2 +1,12 @@
-def test_install():
-    assert 1 == 1
+"""_summary_."""
+import subprocess
+
+from autoverify import __version__ as AV_VERSION
+
+
+def test_version():
+    """Also serves as a test to see if the CLI works at all."""
+    result = subprocess.run(
+        ["auto-verify", "--version"], stdout=subprocess.PIPE
+    )
+    assert str(result.stdout).find(AV_VERSION) >= 0
