8d528afcf815188f64c5bbee01adaadd70697f5f
c <corne.spek15@gmail.com>
2023-04-12 14:12:24 +0200

    WIP rename to tool dir

--- autoverify/cli/install/installers/nnenum.py
+++ autoverify/cli/install/installers/nnenum.py
@@ -16,8 +16,7 @@ def install(install_dir: Path):
     """_summary_."""
     os.chdir(install_dir)
 
-    subprocess.run(NnenumRepoInfo.clone, check=True)
-    os.chdir(install_dir / NnenumRepoInfo.repo_name)
-    subprocess.run(NnenumRepoInfo.checkout, check=True)
-
-    # raise NotImplementedError
+    subprocess.run(NnenumRepoInfo.clone, check=True, capture_output=True)
+    os.rename(install_dir / NnenumRepoInfo.repo_name, install_dir / "tool")
+    os.chdir(install_dir / "tool")
+    subprocess.run(NnenumRepoInfo.checkout, check=True, capture_output=True)
