95d6c73653e93733a5fea84352f55292720fb9bd
c <corne.spek15@gmail.com>
2023-04-17 17:07:32 +0200

    move function to util file

--- tests/test_cli/test_install.py
+++ tests/test_cli/test_install.py
@@ -1,19 +1,12 @@
 """_summary_."""
 # TODO: Dont hardcore error strings, import them from somewhere instead
-import subprocess
 from pathlib import Path
 
 import pytest
 
 import autoverify
 
-
-def run_av_cli(args: list[str]) -> str:
-    base_cmd = ["auto-verify"]
-    base_cmd.extend(args)
-
-    result = subprocess.run(base_cmd, stdout=subprocess.PIPE)
-    return str(result.stdout)
+from ..util import run_av_cli
 
 
 def test_version():
@@ -38,17 +31,3 @@ def test_install(monkeypatch: pytest.MonkeyPatch, tmp_path: Path):
     # Installing the same verifier twice should fail
     output = run_av_cli(["--install", "DummyVerifier"])
     assert output.find("Error initializing") >= 0
-
-
-# TODO: Needs proper cleanup mechanisms, very wonky atm.
-def test_install_nnenum(monkeypatch: pytest.MonkeyPatch, tmp_path: Path):
-    monkeypatch.chdir(tmp_path)
-    monkeypatch.setenv("XDG_DATA_HOME", str(tmp_path))
-
-    output = run_av_cli(["--install", "nnenum"])
-
-    assert output.find("Succesfully installed") >= 0
-
-    output = run_av_cli(["--uninstall", "nnenum"])
-
-    assert output.find("Succesfully uninstalled") >= 0
--- tests/test_verifier/test_nnenum.py
+++ tests/test_verifier/test_nnenum.py
@@ -1,14 +1,31 @@
+from pathlib import Path
+
 import pytest
 from result import Ok
 
 from autoverify.verifier import Nnenum
 
+from ..util import run_av_cli
 from .conftest import VerificationInstance
 
 
 @pytest.fixture
 def nnenum() -> Nnenum:
     return Nnenum()
+
+
+# TODO: Needs proper cleanup mechanisms
+def test_install_nnenum(monkeypatch: pytest.MonkeyPatch, tmp_path: Path):
+    monkeypatch.chdir(tmp_path)
+    monkeypatch.setenv("XDG_DATA_HOME", str(tmp_path))
+
+    output = run_av_cli(["--install", "nnenum"])
+
+    yield output.find("Succesfully installed") >= 0
+
+    output = run_av_cli(["--uninstall", "nnenum"])
+
+    assert output.find("Succesfully uninstalled") >= 0
 
 
 def test_sat(nnenum: Nnenum, trivial_sat: VerificationInstance):
