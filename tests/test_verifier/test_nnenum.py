08ec53074f891b5e49a76db206e88b6228bb5665
c <corne.spek15@gmail.com>
2023-04-20 16:48:04 +0200

    remove install test -- will be handled by docker failing

--- tests/test_verifier/test_nnenum.py
+++ tests/test_verifier/test_nnenum.py
@@ -15,23 +15,6 @@ def nnenum() -> Nnenum:
     return Nnenum()
 
 
-# TODO: Figure out how to do this properly later
-@pytest.mark.install
-def test_install_nnenum(monkeypatch: pytest.MonkeyPatch, tmp_path: Path):
-    monkeypatch.chdir(tmp_path)
-    monkeypatch.setenv("XDG_DATA_HOME", str(tmp_path))
-    #
-    # output = run_av_cli(["--install", "nnenum"])
-    #
-    # assert output.find("Succesfully installed") >= 0
-    #
-    # output = run_av_cli(["--uninstall", "nnenum"])
-    #
-    # assert output.find("Succesfully uninstalled") >= 0
-    #
-    assert 1
-
-
 @pytest.mark.install
 def test_sat(nnenum: Nnenum, trivial_sat: VerificationInstance):
     result = nnenum.verify_property(trivial_sat.property, trivial_sat.network)
