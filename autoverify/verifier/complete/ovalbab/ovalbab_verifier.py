2e6b17b1f4a7e1793e69445b601b21f6a48a7439
c <corne.spek15@gmail.com>
2023-05-02 11:48:01 +0200

    working ovalbab runner

--- autoverify/verifier/complete/ovalbab/ovalbab_verifier.py
+++ autoverify/verifier/complete/ovalbab/ovalbab_verifier.py
@@ -1,9 +1,15 @@
 """Temporary ovalbab verifier."""
 import os
+import subprocess
+import sys
+import tempfile
 from pathlib import Path
 
-from result import Ok
+from result import Err, Ok
 
+from autoverify.util import find_substring
+from autoverify.util.conda import get_conda_path, get_conda_source_cmd
+from autoverify.util.env import get_file_path
 from autoverify.verifier.verification_result import (
     CompleteVerificationOutcome,
     CompleteVerificationResult,
@@ -27,11 +33,68 @@ class OvalBab(CompleteVerifier):
         self, property: Path, network: Path
     ) -> CompleteVerificationResult:
         """_summary_."""
-        # temp silence warnings
         os.chdir(self.tool_path)
 
-        outcome = CompleteVerificationOutcome("SAT", None)
-        return Ok(outcome)
+        result_file = Path(tempfile.NamedTemporaryFile("w").name)
+        config_file = get_file_path(Path(__file__)) / "temp_ovalbab_config.json"
+
+        run_cmd = self._get_runner_cmd(
+            property, network, result_file, config_file
+        )
+
+        try:
+            result = subprocess.run(
+                run_cmd,
+                executable="/bin/bash",
+                capture_output=True,
+                check=True,
+                shell=True,
+            )
+        except subprocess.CalledProcessError as err:
+            print(f"OvalBab Error:\n{err.stderr}")
+            return Err("Exception during call to oval-bab")
+        except Exception as err:
+            print(f"Exception during call to oval-bab, {str(err)}")
+            return Err("Exception during call to oval-bab")
+
+        stdout = result.stdout.decode()
+        verification_outcome = self._parse_result(stdout)
+
+        if isinstance(verification_outcome, CompleteVerificationOutcome):
+            return Ok(verification_outcome)
+        else:
+            return Err("Failed to parse output")
+
+    def _parse_result(
+        self,
+        tool_result: str,
+    ) -> CompleteVerificationOutcome | None:
+        """_summary_."""
+        if find_substring("SAT", tool_result):
+            # TODO: Counterexample
+            return CompleteVerificationOutcome("SAT", None)
+        elif find_substring("UNSAT", tool_result):
+            return CompleteVerificationOutcome("UNSAT")
+
+        return None
+
+    def _get_runner_cmd(
+        self,
+        property: Path,
+        network: Path,
+        result_file: Path,
+        config_file: Path,
+    ) -> str:
+        source_cmd = get_conda_source_cmd(get_conda_path())
+
+        return f"""
+        {" ".join(source_cmd)}
+        conda activate {self.conda_env_name}
+        python tools/bab_tools/bab_from_vnnlib.py --mode run_instance \
+        --onnx {str(network)} --vnnlib {str(property)} \
+        --result_file {str(result_file)} --json {config_file} \
+        --instance_timeout {sys.maxsize}
+        """
 
     def sample_configuration(
         self, config_levels: set[ConfigurationLevel], size: int
