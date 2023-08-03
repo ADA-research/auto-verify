931b0682f5d31b60b727ae106284c39b72c5c3d0
c <corne.spek15@gmail.com>
2023-08-03 11:02:51 +0200

    update nnenum hash [skip ci]

--- autoverify/cli/install/installers/nnenum/install.py
+++ autoverify/cli/install/installers/nnenum/install.py
@@ -7,7 +7,7 @@ from autoverify.util.env import copy_env_file_to
 
 NnenumRepoInfo = GitRepoInfo(
     branch="vnncomp2022",
-    commit_hash="a6eb6db",
+    commit_hash="ec1eb41",
     clone_url="https://github.com/kw-corne/nnenum",
 )
 
