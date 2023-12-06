93d3394c51a4a56bb8e9e6a4f898d669faad0401
c <corne.spek15@gmail.com>
2023-12-06 15:24:18 +0100

    bump version 0.1.2 -> 0.1.3

--- autoverify/__init__.py
+++ autoverify/__init__.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
 DEFAULT_VERIFICATION_TIMEOUT_SEC = 600
